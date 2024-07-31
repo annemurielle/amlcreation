eadc7fe8d8309e6dee5121a242a59059545bd8bc
Larger Anne-Murielle <annemurielle.larger@gmail.com>
2024-07-31 17:36:29 +0200

    Initial commit

---
+++ Namea/Scripts/JQueryBasicSlider.js
@@ -0,0 +1,20 @@
+﻿<!--  Attach the plug-in to the slider parent element and adjust the settings as required -->
+   $(document).ready(function () {
+
+        photoPaneSlider = $('#fade-example').bjqs({
+            'animation': 'slide',
+            'showMarkers': false,
+            'centerControls': true,
+            'automatic': false,
+            'nextText': '<i class="icon-chevron-right icon-white"></i>',
+            'prevText': '<i class="icon-chevron-left icon-white"></i>',
+            'useCaptions': false,
+            'keyboardNav': true,
+            'width': 714,
+
+            'height': 950
+        });
+
+
+
+    });
